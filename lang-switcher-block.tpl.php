186459792ffbe718ab8936b0d3a296763630da7c
Riccardo Franconi <riccardo.franconi@gmail.com>
2013-03-15 01:08:44 +0100

    right naming on tpl functions

--- lang-switcher-block.tpl.php
+++ lang-switcher-block.tpl.php
@@ -5,8 +5,8 @@ $links = language_negotiation_get_switch_links('language_url', $path);
 ?>
 <ul class="language-switcher-locale-url">
   <?php foreach ($languages as $langCode => $language) { ?>
-  <li class="en last <?php print reb_sy_get_active_class_by_language($langCode); ?>">
-      <a href="/<?php print $langCode ?>/<?php print drupal_lookup_path('alias',$links->links[$langCode]['href'], $langCode) ?>" class="language-link <?php print reb_sy_get_active_class_by_language($langCode); ?>" lang="en" title="School">
+  <li class="en last <?php print ricfrank_lang_switcher_get_active_class_by_language($langCode); ?>">
+      <a href="/<?php print $langCode ?>/<?php print drupal_lookup_path('alias',$links->links[$langCode]['href'], $langCode) ?>" class="language-link <?php print ricfrank_lang_switcher_get_active_class_by_language($langCode); ?>" lang="en" title="School">
         <?php print $language->native; ?>
       </a>
     </li>
